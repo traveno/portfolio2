fb1521590dc07317280a84e9accc3b5463d92e2b
Stephen <teamgreensf@gmail.com>
2023-04-13 10:08:35 -0700

    Remove debug info

--- src/lib/components/Demo3D.svelte
+++ src/lib/components/Demo3D.svelte
@@ -18,7 +18,7 @@
     let zoomScale: number | undefined;
 
     $: if (windowWidth && windowHeight) {
-        if (zoomScale === undefined) zoomScale = (windowWidth / windowHeight) < 0.75 ? 0.6 : 1;
+        if (zoomScale === undefined) zoomScale = (windowWidth / windowHeight) < 0.75 ? 0.4 : 1;
     }
 
 
@@ -80,7 +80,7 @@
 <svelte:window bind:innerWidth={windowWidth} bind:innerHeight={windowHeight} />
 
 <div class="relative lg:rounded-lg aspect-video overflow-hidden border-warning">
-    <p>{zoomScale} {(windowWidth / windowHeight)}</p>
+    <!-- <p>{zoomScale} {(windowWidth / windowHeight)}</p> -->
     {#if zoomScale !== undefined}
     <div class="absolute w-full h-full">
         <Canvas>
